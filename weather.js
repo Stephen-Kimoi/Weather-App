30e9247d70446bd13b9dd74a63ca5dfee82269e8
steve kimoi <87986813+Stephen-Kimoi@users.noreply.github.com>
2021-11-27 09:06:55 +0300

    function for displaying the weather data

--- weather.js
+++ weather.js
@@ -1,30 +1,42 @@
 
 class Weather {
-    constructor(location) {
+    constructor() {
         this.apiKey = '3fd44b4056413ee144ec7c7fcef4a9fc'; 
-        this.location = location; 
     } 
 
     //function for fetching weather data 
-    async getWeather() {
-        await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${this.apiKey}`)
+    async getWeather(location) {
+        await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${this.apiKey}`)
            .then((Response) => Response.json())
-           .then((data) => this.displayWeather(data)); 
+           .then((data) =>  { this.displayWeather(data); console.log(data) }); 
     } 
 
+    // Function for displaying the weather data 
+    displayWeather(data) { 
+        const { name } =  data; 
+        const  { temp, humidity } =  data.main; 
+        const { speed } =  data.wind; 
+        const { pressure } =  data.main; 
+        const { icon, description} =  data.weather[0]; 
 
+        document.querySelector('h2').innerHTML = `Weather in ${name}`;
+        document.querySelector('.temp').innerHTML = `${temp}ºC`;
+        document.querySelector('.temp').innerHTML = `${temp}ºC`;
+        document.querySelector('img').src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector('.description').innerHTML = `${description}`;
+        document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}`; 
+        document.querySelector('.wind').innerHTML = `Windspeed: ${speed}km/hr`;
+        
 
-    // Change location function 
-    changeLocation(location) {
-        this.location = location; 
-    } 
-}
+        console.log(name,icon, description, temp, humidity, speed, pressure); 
+    }
+   
+}              
 
 
 // Initialize a new weather object 
-const weather = new Weather('Denver'); 
+const weather = new Weather(); 
 
-weather.getWeather()
 
 
 
